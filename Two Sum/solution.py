5294cbea5b202046bb0a846ecffcf9342959ecdf
Tosshiro <hjianwei1@gmail.com>
2023-10-12 12:10:57 +0800

    Update solution.py

---
+++ Two Sum/solution.py
@@ -0,0 +1,18 @@
+'''
+Description
+LeetCode solution to 1.Two Sum
+'''
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        prevMap = {}  #Map val : index
+
+        # run loop to form list containing tuples of idx & num value
+        for i,n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:    #Testing if two numbers add up to target, else add to dictionary
+                return [prevMap[diff],i]
+            prevMap[n] = i
+
+#Time complexity: O(n)
+#Memory: O(n)
