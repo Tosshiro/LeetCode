5ef32eb058a808601195c995fdfa2f29a1795f51
Tosshiro <hjianwei1@gmail.com>
2023-10-14 23:24:22 +0800

    new problem 242

---
+++ 1. Arrays&Hashing/242_Valid_Anagram.py
@@ -0,0 +1,21 @@
+'''
+Description
+LeetCode solution to 242. Valid Anagram
+'''
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #Check if number of letters of s and t is the same
+        if len(s) != len(t):
+            return False
+
+        lis = [i for i in t]
+        for i in s:
+            if i in lis:   #check if every letter in s is in t
+                lis.remove(i)  #remove checked letter
+            else:
+                return False   #if letter in s not in t, s & t not valid anagram
+        return True
+
+    #Time Complexity: O(n^2)
