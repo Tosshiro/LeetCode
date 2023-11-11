f220a601ddf6cc666a2f870523e4462a10c7d096
Tosshiro <hjianwei1@gmail.com>
2023-11-11 16:44:59 +0800

    Problem 844

---
+++ 4. Stack/844_Backspace_String_Compare.py
@@ -0,0 +1,35 @@
+'''
+Description
+LeetCode solution to 844. Backspace String Compare
+
+Good question to get started with stack and oop
+
+@author: Jw
+'''
+
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        stack1 = []
+        stack2 = []
+
+        for i in s:
+            if i == "#":
+                if len(stack1) != 0:
+                    stack1.pop()
+            else:
+                stack1.append(i)
+
+        for i in t:
+            if i == "#":
+                if len(stack2) != 0:
+                    stack2.pop()
+            else:
+                stack2.append(i)
+
+        if "".join(stack1) == "".join(stack2):
+            return True
+        else:
+            return False
+
+    # Time Complexity: O(n), n = total length of s and t
+    # Space complexity: O(n), if no "#" and whole string stored in stack list
